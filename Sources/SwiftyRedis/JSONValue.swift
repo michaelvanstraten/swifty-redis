a2cce7767516ad7e5d1243f8b0916000ef67d08f
Michael van Straten <michael@vanstraten.de>
2023-05-19 23:12:03 +0200

    Added mock up JSONValue wrapper type

--- Sources/SwiftyRedis/JSONValue.swift
+++ Sources/SwiftyRedis/JSONValue.swift
@@ -1,8 +1,24 @@
 //
 //  File.swift
-//  
+//
 //
 //  Created by Michael van Straten on 02.05.23.
 //
 
 import Foundation
+
+struct JSONValue<T> {
+    let value: T
+}
+
+extension JSONValue: FromRedisValue where T: Decodable {
+    internal init(_ value: RedisValue) throws {
+        switch value {
+        case let .BulkString(string):
+            let decoder = JSONDecoder()
+            self.value = try decoder.decode(T.self, from: string.data(using: .utf8)!)
+        default:
+            throw RedisError.make_invalid_type_error(detail: "Response type json value")
+        }
+    }
+}
