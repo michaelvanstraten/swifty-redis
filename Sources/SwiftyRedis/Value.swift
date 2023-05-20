159dbc4c6012a80e01eb0f380faff4a43d00472f
Michael van Straten <michael@vanstraten.de>
2023-05-20 17:22:43 +0200

    Improved documentation for Value

--- Sources/SwiftyRedis/Value.swift
+++ Sources/SwiftyRedis/Value.swift
@@ -8,19 +8,19 @@
 import Foundation
 
 /**
- The redis value enum parsed from a redis reply.
+ The RedisValue enum represents different types of values parsed from a Redis reply.
+
  Note that Error values are not returned, the corresponding function throws a ``RedisError`` in the case one is returned from redis.
  */
-
 public indirect enum RedisValue: Equatable {
-    /// Simple Strings are used to transmit non binary-safe strings with minimal overhead. For example, many Redis commands reply with just "OK" on success.
+    /// Represents non-binary safe strings with minimal overhead. Typically used for successful command replies. For example, many Redis commands reply with just "OK.
     case SimpleString(String)
-    /// Integer response from server, nothing special. However, the returned integer is guaranteed to be in the range of a signed 64-bit integer.
+    /// Represents integer responses from the server. The returned integer is guaranteed to be within the range of a signed 64-bit integer.
     case Int(Int64)
-    /// Bulk Strings are used in order to represent a single binary-safe string up to 512 MB in length.
+    /// Represents a single binary-safe string up to 512 MB in length.
     case BulkString(String)
-    /// Signals the non-existence of a value
+    /// Represents the absence of a value.
     case Nil
-    /// Certain Redis commands, that return collections of elements to the client, use Arrays as their replies, this the corresponding return case.
+    /// Represents an array of RedisValues, commonly used for commands that return collections of elements.
     case Array([RedisValue])
 }
