a06c0e1539a08edc9e3793ce7fd43a9c68408293
Michael van Straten <michael@vanstraten.de>
2023-05-20 17:18:43 +0200

    Added PubSubConnection tests

---
+++ Tests/SwiftyRedisTests/PubSubConnectionTests.swift
@@ -0,0 +1,64 @@
+//
+//  PubSubConnectionTests.swift
+//  SwiftyRedis
+//
+//  Created by Michael van Straten on 20.05.2023.
+//
+
+import Network
+@testable import SwiftyRedis
+import XCTest
+
+final class PubSubConnectionTests: XCTestCase {
+    let client = RedisClient.LOCAL
+
+    func testPubSubConnection() async throws {
+        let connection = try await client.get_connection()
+
+        let pubSubConnection = try await client.get_pub_sub_connection()
+
+        // Subscribe to a channel
+        try await pubSubConnection.subscribe("channel1")
+
+        // Publish a message to the channel
+        Task {
+            try await Task.sleep(nanoseconds: UInt64(1 * Double(NSEC_PER_SEC)))
+            try await connection.publish("channel1", "Hello, World!")
+        }
+
+        // Receive the message from the PubSubConnection's message stream
+        var messages = await pubSubConnection.messages().makeAsyncIterator()
+        // Ignore `subscribe` message
+        let _ = await messages.next()
+        if let result = await messages.next() {
+            switch result {
+            case let .success(message):
+                XCTAssertEqual(message.channel, "channel1")
+                XCTAssertEqual(message.type, .message(pattern: nil, payload: "Hello, World!"))
+            case let .failure(error):
+                XCTFail("Received error: \(error)")
+            }
+        } else {
+            XCTFail("Failed to receive message")
+        }
+
+        // Unsubscribe from the channel
+        try await pubSubConnection.unsubscribe("channel1")
+    }
+
+    func testPubSubMessageParsing() throws {
+        // Create a sample RedisValue representing a Pub/Sub message
+        let redisValue: RedisValue = .Array([
+            .BulkString("message"),
+            .BulkString("channel1"),
+            .BulkString("Hello, World!"),
+        ])
+
+        // Parse the RedisValue into a PubSubMessage
+        let pubSubMessage = try PubSubMessage(redisValue)
+
+        // Verify the parsed PubSubMessage properties
+        XCTAssertEqual(pubSubMessage.channel, "channel1")
+        XCTAssertEqual(pubSubMessage.type, .message(pattern: nil, payload: "Hello, World!"))
+    }
+}
