f144d8f4b3748d4707d66ae65bccb3e1affba03b
Michael van Straten <michael@vanstraten.de>
2023-05-20 17:21:36 +0200

    Improved documentation for Pipeline

--- Sources/SwiftyRedis/Pipeline.swift
+++ Sources/SwiftyRedis/Pipeline.swift
@@ -8,14 +8,18 @@
 import Foundation
 
 /**
- A pipeline allows you to send multiple commands in one go to the
- redis server. API wise it's very similar to just using a command
- but it allows multiple commands to be chained and some features such
- as iteration are not available.
+ A RedisPipeline allows you to send multiple commands to the Redis server in a single operation.
+
+ It provides a similar API to individual commands but enables chaining multiple commands together.
  */
 public class RedisPipeline {
     private var commands: [Cmd] = []
 
+    /**
+     Packs the pipeline into a binary format that can be sent to the Redis server.
+
+     - Returns: The packed pipeline as ``Data``.
+     */
     internal func pack_pipeline() -> Data {
         var packed_pipeline = Data()
         for cmd in commands {
@@ -24,26 +28,37 @@ public class RedisPipeline {
         return packed_pipeline
     }
 
-    /// Adds a command to the pipeline.
-    /// - Parameter cmd: the Command to add to the pipeline
-    /// - Returns: the pipeline it self
+    /**
+     Adds a command to the pipeline.
+
+     - Parameter cmd: The command to add to the pipeline.
+     - Returns: The pipeline itself.
+     */
     @discardableResult
     public func add_command(_ cmd: Cmd) -> Self {
         commands.append(cmd)
         return self
     }
 
-    /// Starts a new command. Functions such as ``RedisPipeline/arg(_:)`` then become
-    /// available to add more arguments to that command.
+    /**
+     Starts a new command. Additional arguments can be added to the command using the ``RedisPipeline/arg(_:)`` method.
+
+     - Parameter name: The name of the command.
+     - Returns: The pipeline itself.
+     */
     @discardableResult
     public func cmd(_ name: String) -> Self {
         add_command(Cmd(name))
     }
 
-    /// Adds an argument to the last started command. This works similar
-    /// to the ``Cmd/arg(_:)``  method of the ``Cmd`` struct.
-    ///
-    /// Note that this function does nothing if there are no commands in the pipeline.
+    /**
+     Adds an argument to the last started command. This method works similar to the ``Cmd/arg(_:)`` method of the ``Cmd`` struct.
+
+     Note: This function does nothing if there are no commands in the pipeline.
+
+     - Parameter arg: The argument to add to the command.
+     - Returns: The pipeline itself.
+     */
     @discardableResult
     public func arg<T: ToRedisArgs>(_ arg: T) -> Self {
         if let current_cmd = commands.last {
@@ -52,13 +67,26 @@ public class RedisPipeline {
         return self
     }
 
-    /// Sends the Pipeline as query to the connection and converts the result to the target redis value.
+    /**
+     Sends the pipeline as a query to the Redis connection and converts the result to the specified Redis value type.
+
+     - Parameter con: The Redis connection to send the query to.
+     - Returns: The result of the query as the specified Redis value type.
+     - Throws: An error if the query fails.
+     */
     public func query<T: FromRedisValue>(_ con: RedisConnection) async throws -> T {
         let value = try await con.request_packed_cmd(pack_pipeline())
         return try T(value)
     }
 
-    /// This is a shortcut to ``RedisPipeline/query(_:)`` that does not return a ``RedisValue`` and will throw if the query fails because of an error.
+    /**
+     Executes the pipeline as a query to the Redis connection.
+
+     This is a shortcut to the ``query(_:)`` method that does not return a ``RedisValue`` and will throw an error if the query fails.
+
+     - Parameter con: The Redis connection to send the query to.
+     - Throws: An error if the query fails.
+     */
     public func exec(_ con: RedisConnection) async throws {
         let _: RedisValue = try await query(con)
     }
