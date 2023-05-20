9c061796afaf470645e8eacc39c1ac667c2a04bd
Michael van Straten <michael@vanstraten.de>
2023-05-20 17:21:50 +0200

    Improved documentation for ProtocolSpec

--- Sources/SwiftyRedis/ProtocolSpec.swift
+++ Sources/SwiftyRedis/ProtocolSpec.swift
@@ -11,16 +11,21 @@ import Foundation
  Swift representation of [RESP protocol description](https://redis.io/docs/reference/protocol-spec/#resp-protocol-description) constants.
  */
 public enum RedisRESP {
-    /// Different parts of the protocol are always terminated with "\r\n" (CRLF).
-    public static let CRLF = [UInt8(13), UInt8(10)]
-    /// Simple Strings, the first byte of the reply is "+"
-    public static let SimpleString = UInt8(ascii: "+")
-    /// Errors, the first byte of the reply is "-"
-    public static let Error = UInt8(ascii: "-")
-    /// Integers, the first byte of the reply is ":"
-    public static let Int = UInt8(ascii: ":")
-    /// Bulk Strings, the first byte of the reply is "$"
-    public static let BulkString = UInt8(ascii: "$")
-    /// Arrays, the first byte of the reply is "*"
-    public static let Array = UInt8(ascii: "*")
+    /// Represents the CRLF (Carriage Return and Line Feed) sequence used to terminate different parts of the protocol.
+    public static let CRLF: [UInt8] = [13, 10]
+
+    /// Represents the first byte of a Simple String in the RESP protocol, which is "+".
+    public static let SimpleString: UInt8 = .init(ascii: "+")
+
+    /// Represents the first byte of an Error in the RESP protocol, which is "-".
+    public static let Error: UInt8 = .init(ascii: "-")
+
+    /// Represents the first byte of an Integer in the RESP protocol, which is ":".
+    public static let Int: UInt8 = .init(ascii: ":")
+
+    /// Represents the first byte of a Bulk String in the RESP protocol, which is "$".
+    public static let BulkString: UInt8 = .init(ascii: "$")
+
+    /// Represents the first byte of an Array in the RESP protocol, which is "*".
+    public static let Array: UInt8 = .init(ascii: "*")
 }
